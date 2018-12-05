06a0823fcbc0ec8c92d53ae98cbaf3a022ed77a6
Federico D <f.g.defaveri@gmail.com>
2018-12-05 19:23:30 +0000

    Writes functions to run onion scans and handle timeouts

--- onionrunner/onionrunner.py
+++ onionrunner/onionrunner.py
@@ -51,6 +51,7 @@ def get_onion_list():
 # Stores an onion in the list of onions
 #
 def store_onion(onion):
+    
     print("[++] Storing %s in master list." % onion)
 
     with codecs.open("onion_list.txt", "ab", encoding="utf8") as fd:
@@ -58,10 +59,80 @@ def store_onion(onion):
 
     return
 
+#
+# Runs onionscan as a child process
+#
+def run_onionstan(onion):
+    
+    print("[*] Onionscanning %s" % onion)
+
+    #fire up onionscan
+    process = subprocess.Popen(["onionscan", "webport=0", "--jsonreport", "--simplereport=false", onion], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    #start the timer for 5 minutes
+    process_timer = Timer(300, handle_timeout, args=[process, onion])
+    process_timer.start()
+
+    #wait for the onion scan results
+    stdout = process.communicate()[0]
+
+    #if the results are valid, kill the timer
+    if process_timer.is_alive():
+        process_timer.cancel()
+        return stdout
+
+    print("[!!] Process timed out!")
+
+    return None
+
+#
+# Handles timer timeout from the onionscan proces
+#
+def hadle_timeout(process, onion):
+    global session_onions
+    global identity_lock
+    
+    #halt the main thread while we grab a new identity
+    identity_lock.clear()
+    
+    #kill onionscan process
+    try:
+        process.kill()
+        print("[!!] Killed the onionscan process.")
+    except:
+        pass
+
+    #switch Tor identities to guarantee that we have good connection
+    with Controller.from_port(port=9051) as torcontrol:
+
+        #auth
+        torcontrol.authenticate(os.environ("ONIONRUNNER_PW") )
+
+        #send the signal for a new identity
+        torcontrol.signal(Signal.NEWNYM)
+
+        #wait for initialization of new identity
+        time.sleep(torcontrol.get_newnym_wait() )
+
+        print("[!!] Switched Tor identities")
+
+    #push the onion back to the list
+    session_onions.append(onion)
+    random.shuffle(session_onions)
+
+    #allow the main thread to resum executing
+    identity_lock.set()
+
+    return
 
 
 
-print("Hello welcome")
+#
+# MAIN
+#
+
+print("Hello welcome " + os.environ('ONIONRUNNER_PW') )
+
 get_onion_list()
 
 
